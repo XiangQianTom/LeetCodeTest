741190aa2942e321a6a95370cd16ef3b6197e755
sixiangqian <sixiangqian@ainirobot.com>
2021-03-02 19:42:09 +0800

    26. 删除排序数组中的重复项

---
+++ src/S26_RemoveDuplicates.java
@@ -0,0 +1,116 @@
+import utils.ListNode;
+
+public class S26_RemoveDuplicates {
+    /**
+     * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+     * <p>
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     * <p>
+     * <p>
+     * <p>
+     * 示例1:
+     * <p>
+     * 给定数组 nums = [1,1,2],
+     * <p>
+     * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+     * <p>
+     * 你不需要考虑数组中超出新长度后面的元素。
+     * 示例2:
+     * <p>
+     * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+     * <p>
+     * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+     * <p>
+     * 你不需要考虑数组中超出新长度后面的元素。
+     * <p>
+     * <p>
+     * 说明:
+     * <p>
+     * 为什么返回数值是整数，但输出的答案是数组呢?
+     * <p>
+     * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+     * <p>
+     * 你可以想象内部操作如下:
+     * <p>
+     * // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+     * int len = removeDuplicates(nums);
+     * <p>
+     * // 在函数里修改输入数组对于调用者是可见的。
+     * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+     * for (int i = 0; i < len; i++) {
+     * print(nums[i]);
+     * }
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] ints = {1, 1, 2, 3, 3, 4, 4};
+        int length = solution.removeDuplicates(ints);
+        System.out.println(length);
+        for (int i = 0; i < length; i++) {
+            System.out.print(ints[i] + " ");
+        }
+    }
+
+    static class Solution {
+        public int removeDuplicates1(int[] nums) {
+            int length = nums.length;
+            if (length <= 1) {
+                return length;
+            }
+            int temp = nums[0];
+            int index = 1;
+            for (int i = 1; i < length; i++) {
+                while (i < length && nums[i] == temp) {
+                    i++;
+                }
+                if (i == length) {
+                    return index;
+                }
+                nums[index] = nums[i];
+                temp = nums[i];
+                index++;
+            }
+            return index;
+        }
+
+        public int removeDuplicates2(int[] nums) {
+            int length = nums.length;
+            if (length <= 1) {
+                return length;
+            }
+            int temp = nums[0];
+            int index = 1;
+            for (int i = 1; i < length; ) {
+                if (nums[i] == temp) {
+                    i++;
+                    continue;
+                }
+                nums[index] = nums[i];
+                temp = nums[i];
+                index++;
+            }
+            return index;
+        }
+
+        public int removeDuplicates(int[] nums) {
+            int length = nums.length;
+            if (length <= 1) {
+                return length;
+            }
+            int index = 0;
+            for (int i = 1; i < length; i++) {
+                if (nums[i] != nums[index]) {
+                    index++;
+                    nums[index] = nums[i];
+                }
+            }
+            return index + 1;
+        }
+    }
+}
