c54037ac3090f80be84ab4445abfa82f2a8c0ad0
sixiangqian <sixiangqian@orionstar.com>
2023-08-20 01:10:11 +0800

    235. 二叉搜索树的最近公共祖先

---
+++ src/S235_lowestCommonAncestor.java
@@ -0,0 +1,34 @@
+import utils.TreeNode;
+
+public class S235_lowestCommonAncestor {
+    /**
+     * 235. 二叉搜索树的最近公共祖先
+     * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+     * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+     * 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+     * 示例 1:
+     * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+     * 输出: 6
+     * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+     * 示例 2:
+     * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+     * 输出: 2
+     * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+     * https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/
+     */
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lowestCommonAncestor(TreeNode.buildTree(new Integer[]{6, 2, 8, 0, 4, 7, 9, null, null, 3, 5}), new TreeNode(2), new TreeNode(8)).val);
+        System.out.println(solution.lowestCommonAncestor(TreeNode.buildTree(new Integer[]{6, 2, 8, 0, 4, 7, 9, null, null, 3, 5}), new TreeNode(2), new TreeNode(4)).val);
+        System.out.println(solution.lowestCommonAncestor(TreeNode.buildTree(new Integer[]{0, -1000000000, 1000000000}), new TreeNode(-1000000000), new TreeNode(1000000000)).val);
+    }
+
+    static class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if ((long) (root.val - p.val) * (root.val - q.val) <= 0) {
+                return root;
+            }
+            return lowestCommonAncestor(p.val < root.val ? root.left : root.right, p, q);
+        }
+    }
+}
